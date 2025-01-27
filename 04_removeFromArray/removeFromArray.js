f6f7cf28025f4cad17474ff68900e82b851fccc1
Shygun38 <144661332+Shygun38@users.noreply.github.com>
2025-01-27 13:57:24 +1300

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = [];
+    for (const arrItem of array) {
+        let same = false;
+        for (const argItem of arguments) {
+            if (arrItem === argItem) {
+                same = true;
+            }
+        }
+        if (same === false) {
+            newArray.push(arrItem);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
