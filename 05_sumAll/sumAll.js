8625d3ac1baa5b1a01fd5664a5f7bb79a7055f83
Shygun38 <144661332+Shygun38@users.noreply.github.com>
2025-02-06 14:32:25 +1300

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number'
+    || num1 < 0 || num2 < 0
+    || num1 % 1 !== 0 || num2 % 1 !== 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    let start;
+    let end;
+    if (num1 < num2) {
+        start = num1;
+        end = num2;
+    } else {
+        start = num2;
+        end = num1;
+    }
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
